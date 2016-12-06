16f27c8b61d5902b0ed77f75d9a79785dfce58b5
konglee <konglee873@gmail.com>
2016-12-06 19:56:54 +0800

    关闭WKWebView的Forward手势

--- CloseWKGesture/ViewController.m
+++ CloseWKGesture/ViewController.m
@@ -7,23 +7,65 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
+#import <objc/runtime.h>
+#define KWidth [UIScreen mainScreen].bounds.size.width
+#define KHeight [UIScreen mainScreen].bounds.size.height
 
-@interface ViewController ()
+@interface ViewController ()<WKNavigationDelegate>
+{
+    WKWebView *_webView;
+}
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initUI];
+}
+
+- (void)initUI
+{
+    _webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    _webView.allowsBackForwardNavigationGestures = YES;
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://m.aoyou.com"]]];
+    _webView.navigationDelegate = self;
+    
+    id arr = [_webView valueForKey:@"gestureRecognizers"];
+    if([arr isKindOfClass:[NSArray class]])
+    {
+        id gesture = arr[1];
+        if ([gesture isKindOfClass:[UIScreenEdgePanGestureRecognizer class]])
+        {
+            UIScreenEdgePanGestureRecognizer *pan = (UIScreenEdgePanGestureRecognizer *)gesture;
+            pan.enabled = NO;
+        }
+    }
+    [self.view addSubview:_webView];
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSString *urlString = navigationAction.request.URL.absoluteString.lowercaseString;
+
+    
+    if ([urlString containsString:@"aoyou.com"])
+    {
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
+    else
+    {
+        decisionHandler(WKNavigationActionPolicyCancel);
+    }
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 
 @end
