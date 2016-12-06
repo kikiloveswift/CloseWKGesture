569c10700180d5d8a6c0ec03aa4097d1c3b76850
konglee <konglee873@gmail.com>
2016-12-06 18:42:39 +0800

    Initial Commit

---
+++ CloseWKGesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloseWKGesture
+//
+//  Created by kong on 16/12/6.
+//  Copyright © 2016年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
